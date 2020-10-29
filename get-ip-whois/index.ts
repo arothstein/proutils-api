a721906bcfca2ca5f93894437d311af033afe601
Aaron Rothstein <aaron.rothstein@wunita.com>
2020-10-29 12:53:01 -0500

    Working get-ip-whois

--- get-ip-whois/index.ts
+++ get-ip-whois/index.ts
@@ -6,43 +6,22 @@ const httpTrigger: AzureFunction = async function (
 ): Promise<void> {
   try {
     const axios = require("axios").default;
-    const xml2js = require("xml2js");
 
-    axios
-      .get("http://whois.arin.net/rest/ip/" + req.params.ip)
-      .then(function (response) {
-        // handle success
-        // TODO convert xml body to json
-        let resBody = xml2js
-          .parseString(response, (err, result) => {
-            if (err) {
-              throw err;
-            }
-            // `result` is a JavaScript object; convert it to a JSON string
-            const json = JSON.stringify(result, null, 4);
-            console.log(json);
-            return json;
-          })
-          .catch(function (error) {
-            // handle error
-            console.log(error);
-          })
-          .then(function () {
-            // always executed
-          });
+    const res = await axios.get("http://whois.arin.net/rest/ip/" + req.params.ip, { headers: {
+    'Accept': 'application/json'
+  }});
+    console.log(res.data);
 
-        /* Insert logic here */
-
-        context.res = {
+    context.res = {
           // status: 200, /* Defaults to 200 */
           headers: { "Content-Type": "application/json" },
-          body: resBody,
+          body: res.data,
         };
-      });
+
   } catch (err) {
     context.res = {
       status: 500,
-      body: err.message,
+      body: err.message
     };
   }
 };
